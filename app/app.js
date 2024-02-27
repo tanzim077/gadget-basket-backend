62889bedd4b4fd98301bd13f7fcecda03a0ec4aa
tanzim077 <tanzimahmed1994@gmail.com>
2024-02-28 01:40:40 +0600

    Refactor app.js and update webhook endpoint

--- app/app.js
+++ app/app.js
@@ -1,20 +1,19 @@
-import dotenv from "dotenv";
 import cors from "cors";
-import Stripe from "stripe";
-dotenv.config();
+import dotenv from "dotenv";
 import express from "express";
-import path from "path";
+import Stripe from "stripe";
 import dbConnect from "../config/dbConnect.js";
 import { globalErrhandler, notFound } from "../middlewares/globalErrHandler.js";
+import Order from "../model/Order.js";
 import brandsRouter from "../routes/brandsRouter.js";
 import categoriesRouter from "../routes/categoriesRouter.js";
 import colorRouter from "../routes/colorRouter.js";
+import couponsRouter from "../routes/couponsRouter.js";
 import orderRouter from "../routes/ordersRouter.js";
 import productsRouter from "../routes/productsRoute.js";
 import reviewRouter from "../routes/reviewRouter.js";
 import userRoutes from "../routes/usersRoute.js";
-import Order from "../model/Order.js";
-import couponsRouter from "../routes/couponsRouter.js";
+dotenv.config();
 
 //db connect
 dbConnect();
@@ -26,64 +25,58 @@ app.use(cors());
 const stripe = new Stripe(process.env.STRIPE_KEY);
 
 // This is your Stripe CLI webhook secret for testing your endpoint locally.
-const endpointSecret =
-  "whsec_2a222b6d6b7abb9982f25d1da9e63f4d0a78f6935259e4ff65cae8df7b5fdde5";
 
-app.post(
-  "/webhook",
-  express.raw({ type: "application/json" }),
-  async (request, response) => {
-    const sig = request.headers["stripe-signature"];
+app.post("/webhook", express.raw({ type: "application/json" }), async (request, response) => {
+  const sig = request.headers["stripe-signature"];
 
-    let event;
+  let event;
 
-    try {
-      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-      console.log("event");
-    } catch (err) {
-      console.log("err", err.message);
-      response.status(400).send(`Webhook Error: ${err.message}`);
-      return;
-    }
-    if (event.type === "checkout.session.completed") {
-      //update the order
-      const session = event.data.object;
-      const { orderId } = session.metadata;
-      const paymentStatus = session.payment_status;
-      const paymentMethod = session.payment_method_types[0];
-      const totalAmount = session.amount_total;
-      const currency = session.currency;
-      //find the order
-      const order = await Order.findByIdAndUpdate(
-        JSON.parse(orderId),
-        {
-          totalPrice: totalAmount / 100,
-          currency,
-          paymentMethod,
-          paymentStatus,
-        },
-        {
-          new: true,
-        }
-      );
-      console.log(order);
-    } else {
-      return;
-    }
-    // // Handle the event
-    // switch (event.type) {
-    //   case "payment_intent.succeeded":
-    //     const paymentIntent = event.data.object;
-    //     // Then define and call a function to handle the event payment_intent.succeeded
-    //     break;
-    //   // ... handle other event types
-    //   default:
-    //     console.log(`Unhandled event type ${event.type}`);
-    // }
-    // Return a 200 response to acknowledge receipt of the event
-    response.send();
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_SECRET);
+    console.log("event");
+  } catch (err) {
+    console.log("err", err.message);
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
   }
-);
+  if (event.type === "checkout.session.completed") {
+    //update the order
+    const session = event.data.object;
+    const { orderId } = session.metadata;
+    const paymentStatus = session.payment_status;
+    const paymentMethod = session.payment_method_types[0];
+    const totalAmount = session.amount_total;
+    const currency = session.currency;
+    //find the order
+    const order = await Order.findByIdAndUpdate(
+      JSON.parse(orderId),
+      {
+        totalPrice: totalAmount / 100,
+        currency,
+        paymentMethod,
+        paymentStatus,
+      },
+      {
+        new: true,
+      }
+    );
+    console.log(order);
+  } else {
+    return;
+  }
+  // // Handle the event
+  // switch (event.type) {
+  //   case "payment_intent.succeeded":
+  //     const paymentIntent = event.data.object;
+  //     // Then define and call a function to handle the event payment_intent.succeeded
+  //     break;
+  //   // ... handle other event types
+  //   default:
+  //     console.log(`Unhandled event type ${event.type}`);
+  // }
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
 
 //pass incoming data
 app.use(express.json());
@@ -91,11 +84,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //server static files
-app.use(express.static("public"));
+// app.use(express.static("public"));
 //routes
 //Home route
 app.get("/", (req, res) => {
-  res.sendFile(path.join("public", "index.html"));
+  res.status(200).send("API is running");
 });
 app.use("/api/v1/users/", userRoutes);
 app.use("/api/v1/products/", productsRouter);
